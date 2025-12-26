2c30017e426149256d619bdfe73d8a7e641ccda9
Federico Grandi <fgrandi30@gmail.com>
2025-12-26 14:35:16 +0100

    Add empty tRPC router to fix build

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,14 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { emptyRouter } from "./routers/empty";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  empty: emptyRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
